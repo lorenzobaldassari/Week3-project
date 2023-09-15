3914e1c773e1f039e8d827dd5d91593d1a3d0a83
Lorenzo Baldassari <lorebalda93@gmail.com>
2023-09-15 17:07:49 +0200

    penultmio ex corretto

--- script/D10.js
+++ script/D10.js
@@ -577,12 +577,15 @@ halfTree(5);
   *****
 
 */
-let str1 = ` *`;
+let str1 = "*";
 
 const Tree = (n1) => {
   for (i = 0; i < n1 + 1; i++) {
     for (j = 0; j < i; j++) {
-      str1 += `*`.repeat(3);
+      str1 += `**`;
+    }
+    for (j = 0; j < i; j++) {
+      str1 += `*`;
     }
     str1 += "\n";
   }
